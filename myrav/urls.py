898abf73c98f7848e26d5728e39f6a1112061d34
Gaurav s. <gauravsethi4627@gmail.com>
2025-04-17 01:15:58 +0530

    Revert "Implement code changes to enhance functionality and improve performance"
    This reverts commit 13c049e041d43eaa31b875cec9951f53047f262b.

--- myrav/urls.py
+++ myrav/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('ru.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+                               document_root=settings.MEDIA_ROOT)
--- ru/models.py
+++ ru/models.py
@@ -3,11 +3,6 @@ from django.utils import timezone
 from datetime import timedelta
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-
-class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    file_overwrite = False
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True)
