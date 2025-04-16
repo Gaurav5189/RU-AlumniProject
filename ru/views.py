b234b033be41fc39936c3f1d5eb7f12bdeb8b317
Gaurav s. <gauravsethi4627@gmail.com>
2025-04-16 18:54:17 +0530

    Revert "Add a blank line for improved readability in views.py"
    This reverts commit dfb83f0bb001dfa3747e9134b7c374401f1698d0.

--- ru/views.py
+++ ru/views.py
@@ -14,7 +14,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
 
-
 User = get_user_model()  # This will be 'ru.CustomUser' per your AUTH_USER_MODEL
 
 def generate_otp():
