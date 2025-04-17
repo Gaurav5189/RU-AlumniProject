42b9b65c9898283bbf22615f5a41f19b8a7c1141
Gaurav s. <gauravsethi4627@gmail.com>
2025-04-18 04:16:23 +0530

    Refactor static file handling and update deployment scripts for improved build process

--- myrav/settings.py
+++ myrav/settings.py
@@ -152,12 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'  # Note the leading slash
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+if os.environ.get('DISABLE_COLLECTSTATIC') != '1':
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 
 
--- vercel-build-step.sh
+++ vercel-build-step.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Building static files..."
+# Install dependencies
 pip install -r requirements.txt
+# Run collectstatic
 python manage.py collectstatic --noinput
